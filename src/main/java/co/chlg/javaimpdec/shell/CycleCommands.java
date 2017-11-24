0d0c00676b21a25b1267dd2eba678538bed6b92e
Frank Cardona <frankdcm+git@gmail.com>
2017-11-23 22:28:26 -0500

    Other ways of implict cycles

--- src/main/java/co/chlg/javaimpdec/shell/CycleCommands.java
+++ src/main/java/co/chlg/javaimpdec/shell/CycleCommands.java
@@ -9,6 +9,7 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.IntStream;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
@@ -28,15 +29,21 @@ public class CycleCommands {
   @ShellMethod(group = "cycle", value = "Ejercicio de comparación")
   private String[] doNameScores(@ShellOption String nameA, @ShellOption String nameB,
       @ShellOption List<Long> scoresA, @ShellOption List<Long> scoresB) {
+    /*
     List<String> res = new LinkedList<>();
     Iterator<Long> iterB = scoresB.iterator();
     scoresA.forEach(a -> res.add(a > iterB.next() ? nameA : nameB));
     return res.toArray(new String[res.size()]);
+    */
+    return IntStream.range(0, scoresA.size())
+        .mapToObj(i -> scoresA.get(i) > scoresB.get(i) ? nameA : nameB)
+        .toArray(String[]::new);
   }
 
   @ShellMethod(group = "cycle", value = "Ejercicio de ordenamiento")
   private List<String> doSortLastnames(@ShellOption List<String> people) {
-    return people.stream().sorted(comparing(x -> substringAfter(x, "-")))
+    return people.stream()
+        .sorted(comparing(x -> substringAfter(x, "-")))
         .collect(toList());
   }
 
