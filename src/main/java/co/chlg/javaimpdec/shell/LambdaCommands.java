02624f40b9ddff3fc2f3e0254d0cba222012ef69
faltdor <faltdor@gmail.com>
2017-12-04 14:55:35 -0500

    Implementation of the doUpdateVocalCount method

--- src/main/java/co/chlg/javaimpdec/shell/LambdaCommands.java
+++ src/main/java/co/chlg/javaimpdec/shell/LambdaCommands.java
@@ -1,6 +1,7 @@
 package co.chlg.javaimpdec.shell;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import org.springframework.shell.standard.ShellComponent;
@@ -9,28 +10,31 @@ import org.springframework.shell.standard.ShellOption;
 
 @ShellComponent
 public class LambdaCommands {
-  // TODO: Declare concurrent objects...
+	// TODO: Declare concurrent objects...
+	private AtomicInteger total = new AtomicInteger();
 
-  @ShellMethod(group = "lambda", value = "Ejercicio de SAM y lambda")
-  private long doMultPairs(@ShellOption List<Integer> params) {
-    return params.stream()
-        .filter(x -> x % 2 == 0)
-        .map(Long::valueOf)
-        .reduce(1L, (a, b) -> a * b);
-  }
+	@ShellMethod(group = "lambda", value = "Ejercicio de SAM y lambda")
+	private long doMultPairs(@ShellOption List<Integer> params) {
+		return params.stream().filter(x -> x % 2 == 0).map(Long::valueOf).reduce(1L, (a, b) -> a * b);
+	}
 
-  @ShellMethod(group = "lambda", value = "Ejercicio de concurrencia y lambda")
-  private int doUpdateVocalCount(@ShellOption List<String> words) {
-    Predicate<Character> isVocal = null;
-    // TODO: Can use string.chars().mapToObj with monad method and isVocal to filter in the implementation
-    return 0;
-  }
+	@ShellMethod(group = "lambda", value = "Ejercicio de concurrencia y lambda")
+	private int doUpdateVocalCount(@ShellOption List<String> words) {
+		// Predicate<String> isVocal = null;
+		// TODO: Can use string.chars().mapToObj with monad method and isVocal to filter in the implementation
+		int result = words.stream()
+						   .mapToInt(word -> word.replaceAll("(?i)[^aeiouáéíóú]", "").length())
+						   .sum();
 
-  @ShellMethod(group = "lambda", value = "Ejercicio de sincronia y lambda")
-  private List<Boolean> doAllowEntry(@ShellOption List<String> party) {
-    Supplier<Boolean> releaseAndNull = null;
-    // TODO: Can implement releaseAndNull supplier to help map easily in the implementation
-    return null;
-  }
+		return total.addAndGet(result);
+	}
+
+	@ShellMethod(group = "lambda", value = "Ejercicio de sincronia y lambda")
+	private List<Boolean> doAllowEntry(@ShellOption List<String> party) {
+		Supplier<Boolean> releaseAndNull = null;
+		// TODO: Can implement releaseAndNull supplier to help map easily in the
+		// implementation
+		return null;
+	}
 
 }
