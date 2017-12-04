cef6c5adf8f3ce3d482dc6546abb73671808e2ee
Frank Cardona <frankdcm+git@gmail.com>
2017-12-04 17:55:35 -0500

    Adding evaluation responses

--- src/main/java/co/chlg/javaimpdec/rest/MonadController.java
+++ src/main/java/co/chlg/javaimpdec/rest/MonadController.java
@@ -1,7 +1,9 @@
 package co.chlg.javaimpdec.rest;
 
+import static java.lang.Math.pow;
 import static java.util.Arrays.stream;
 import static java.util.Collections.singletonMap;
+import static java.util.function.Predicate.isEqual;
 import static java.util.stream.Stream.generate;
 import static java.util.stream.Stream.iterate;
 import static org.apache.commons.lang3.RandomUtils.nextInt;
@@ -21,21 +23,24 @@ public class MonadController {
   private Map<Integer, Integer> getMappingNumRandomsCount(@PathVariable("luckyNum") int luckyNum,
       @PathVariable("exp") int exp, @PathVariable("qty") int qty) {
     return singletonMap(luckyNum,
-        (int) generate(() -> nextInt(0, (int) Math.pow(10, exp))).limit(qty)
-            .filter(x -> x == luckyNum)
+        (int) generate(() -> nextInt(0, (int) pow(10, exp))).limit(qty)
+            .filter(isEqual(luckyNum))
             .count());
   }
 
   @GetMapping("/map-fibonacci/{num}")
   private Map<Integer, Integer> getMappingNumFib(@PathVariable("num") int num) {
     // TODO: Pair, skip, findFirst will help you get the job done
-    return null;
+    return singletonMap(num,
+        iterate(Pair.of(0, 1), vals -> Pair.of(vals.getRight(), vals.getRight() + vals.getLeft()))
+            .limit(num + 1).skip(num).findFirst().map(Pair::getValue).orElse(null));
   }
 
   @GetMapping("/map-age/{fullName}")
   private Map<String, Integer> getMappingNameAge(@PathVariable("fullName") String fullName) {
     // TODO: sequential & reduce will be useful
-    return null;
+    return singletonMap(fullName,
+        stream(fullName.split(" ")).mapToInt(String::length).reduce(0, (a, b) -> a * 10 + b));
   }
 
 }
--- src/main/java/co/chlg/javaimpdec/shell/LambdaCommands.java
+++ src/main/java/co/chlg/javaimpdec/shell/LambdaCommands.java
@@ -1,6 +1,11 @@
 package co.chlg.javaimpdec.shell;
 
+import static java.util.stream.Collectors.toList;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import org.springframework.shell.standard.ShellComponent;
@@ -9,7 +14,9 @@ import org.springframework.shell.standard.ShellOption;
 
 @ShellComponent
 public class LambdaCommands {
-  // TODO: Declare concurrent objects...
+
+  private final AtomicInteger count = new AtomicInteger();
+  private final Semaphore pass = new Semaphore(2);
 
   @ShellMethod(group = "lambda", value = "Ejercicio de SAM y lambda")
   private long doMultPairs(@ShellOption List<Integer> params) {
@@ -21,16 +28,27 @@ public class LambdaCommands {
 
   @ShellMethod(group = "lambda", value = "Ejercicio de concurrencia y lambda")
   private int doUpdateVocalCount(@ShellOption List<String> words) {
-    Predicate<Character> isVocal = null;
+    Predicate<Character> isVocal = ch -> Arrays
+        .asList('a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú').contains(ch);
     // TODO: Can use string.chars().mapToObj with monad method and isVocal to filter in the implementation
-    return 0;
+    return count.addAndGet(
+        (int) words.stream()
+            .flatMapToInt(String::chars)
+            .mapToObj(x -> (char) x)
+            .filter(isVocal)
+            .count());
   }
 
   @ShellMethod(group = "lambda", value = "Ejercicio de sincronia y lambda")
   private List<Boolean> doAllowEntry(@ShellOption List<String> party) {
-    Supplier<Boolean> releaseAndNull = null;
+    Supplier<Boolean> releaseAndNull = () -> {
+      pass.release();
+      return null;
+    };
     // TODO: Can implement releaseAndNull supplier to help map easily in the implementation
-    return null;
+    return party.stream()
+        .map(x -> x.equals("...") ? releaseAndNull.get() : pass.tryAcquire())
+        .collect(toList());
   }
 
 }
