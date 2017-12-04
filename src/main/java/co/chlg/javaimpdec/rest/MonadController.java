b1a56716a5bf69b8039d96cf827100325aa85091
ajcastanom <ajcastanom@gmail.com>
2017-12-03 19:55:18 -0500

    Se resuelve ejercicio para generación de la serie fibonacci.

--- src/main/java/co/chlg/javaimpdec/rest/MonadController.java
+++ src/main/java/co/chlg/javaimpdec/rest/MonadController.java
@@ -6,7 +6,10 @@ import static java.util.stream.Stream.generate;
 import static java.util.stream.Stream.iterate;
 import static org.apache.commons.lang3.RandomUtils.nextInt;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.IntStream;
+
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,7 +32,11 @@ public class MonadController {
   @GetMapping("/map-fibonacci/{num}")
   private Map<Integer, Integer> getMappingNumFib(@PathVariable("num") int num) {
     // TODO: Pair, skip, findFirst will help you get the job done
-    return null;
+    Map<Integer, Integer> fibonacci = new HashMap<>();
+    fibonacci.put(-1,0);
+    fibonacci.put(0,1);
+    IntStream.range(0, num).forEach(n -> fibonacci.put(n + 1, fibonacci.get(n) + fibonacci.get(n - 1)));
+    return fibonacci;
   }
 
   @GetMapping("/map-age/{fullName}")
