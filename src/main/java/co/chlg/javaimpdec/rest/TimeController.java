a1d36212d9fc6eacd56ea9efc47725e6aee84c1e
Frank Cardona <frankdcm+git@gmail.com>
2017-12-04 19:17:53 -0500

    Adding Time exercise

--- src/main/java/co/chlg/javaimpdec/rest/TimeController.java
+++ src/main/java/co/chlg/javaimpdec/rest/TimeController.java
@@ -1,10 +1,23 @@
 package co.chlg.javaimpdec.rest;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.util.stream.IntStream;
+import org.apache.commons.lang3.RandomUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/time")
 public class TimeController {
+
+  @GetMapping("/duration-rand/{quantity}")
+  private String getRandomQuantityDuration(@PathVariable("quantity") int quantity) {
+    Instant begin = Instant.now();
+    IntStream.generate(RandomUtils::nextInt).limit(quantity).count();
+    return Duration.between(begin, Instant.now()).toString();
+  }
 
 }
--- src/test/java/co/chlg/javaimpdec/rest/TimeControllerTest.java
+++ src/test/java/co/chlg/javaimpdec/rest/TimeControllerTest.java
@@ -7,14 +7,19 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
 import co.chlg.javaimpdec.TestApplicationRunner;
 import java.net.URI;
+import java.time.Duration;
+import java.util.Map;
+import java.util.Set;
 import org.apache.log4j.Logger;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -33,6 +38,32 @@ public class TimeControllerTest {
   @Before
   public void setUp() {
     url = URI.create("http://localhost:" + port + "/time/");
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void getThousandRandomDuration() {
+    // Given...
+    URI uri = url.resolve("duration-rand/" + (2000 * 1000 * 1000));
+    // When...
+    ResponseEntity<String> response = testRest.getForEntity(uri, String.class);
+    // Then...
+    assertThat(response.getStatusCode(), is(OK));
+    assertThat(response.getBody(), instanceOf(String.class));
+    log.info(response.getBody());
+  }
+
+  @Test
+  @SuppressWarnings("unchecked")
+  public void getDayAfter10Hours() {
+    // Given...
+    URI uri = url.resolve("add-hours-return-day/" + 10);
+    // When...
+    ResponseEntity<Integer> response = testRest.getForEntity(uri, Integer.class);
+    // Then...
+    assertThat(response.getStatusCode(), is(OK));
+    assertThat(response.getBody(), is(5));
+    log.info(response.getBody());
   }
 
 }
